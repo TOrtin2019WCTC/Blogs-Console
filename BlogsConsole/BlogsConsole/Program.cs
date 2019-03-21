a41cefd60b0d5b0f968943e14aedeaa383f84a09
tortin2019wctc <tortin@my.wctc.edu>
2019-03-21 13:09:13 -0500

    Added menu functionality

--- BlogsConsole/BlogsConsole/Program.cs
+++ BlogsConsole/BlogsConsole/Program.cs
@@ -11,35 +11,70 @@ namespace BlogsConsole
         public static void Main(string[] args)
         {
             logger.Info("Program started");
+            string ans;
+            var db = new BloggingContext();
+
             try
             {
 
-                // Create and save a new Blog
-                Console.Write("Enter a name for a new Blog: ");
-                var name = Console.ReadLine();
-
-                var blog = new Blog { Name = name };
-
-                var db = new BloggingContext();
-                db.AddBlog(blog);
-                logger.Info("Blog added - {name}", name);
-
-                // Display all Blogs from the database
-                var query = db.Blogs.OrderBy(b => b.Name);
-
-                Console.WriteLine("All blogs in the database:");
-                foreach (var item in query)
+                do
                 {
-                    Console.WriteLine(item.Name);
-                }
+
+                    Console.WriteLine("----------------------------");
+                    Console.WriteLine("\nEnter 1 to ADD BLOG");
+                    Console.WriteLine("Enter 2 to VIEW ALL BLOGS");
+                    Console.WriteLine("Enter 3 to ADD POST TO BLOG");
+                    Console.WriteLine("Enter QUIT to exit");
+                    ans = Console.ReadLine();
+
+                    switch (ans)
+                    {
+                        case "1":
+
+                            logger.Info("Choice: Create Blog");
+                            // Create and save a new Blog
+                            Console.Write("Enter a name for a new Blog: ");
+                            var name = Console.ReadLine().ToLower();
+
+                            var blog = new Blog { Name = name };
+
+
+                            db.AddBlog(blog);
+                            logger.Info("Blog added - {name}", name);
+
+                            break;
+
+                        case "2":
+
+                            logger.Info("Choice: View All Blogs");
+                            // Display all Blogs from the database
+                            var query = db.Blogs.OrderBy(b => b.Name);
+
+                            Console.WriteLine("All blogs in the database:");
+                            foreach (var item in query)
+                            {
+                                Console.WriteLine(item.Name);
+                            }
+
+                            break;
+
+                        case "3":
+                            logger.Info("Choice: Create New Post");
+                            Console.WriteLine("Nothing in here yet");
+
+                            break;
+
+                    }
+
+                } while (!ans.Equals("quit"));
+
+
+
             }
             catch (Exception ex)
             {
                 logger.Error(ex.Message);
             }
-
-            Console.WriteLine("Press enter to quit");
-            string x = Console.ReadLine();
 
             logger.Info("Program ended");
         }
